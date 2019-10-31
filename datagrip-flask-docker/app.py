86b565a77f9afec3ec2a27f4cb6ba3d35bd9618b
Paul Everitt <pauleveritt@me.com>
2019-10-31 16:51:10 -0400

    Replace SQLA with raw psycopg2

--- datagrip-flask-docker/app.py
+++ datagrip-flask-docker/app.py
@@ -1,18 +1,16 @@
 import os
 
+import psycopg2
 from dotenv import load_dotenv
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import text
 
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['TEMPLATES_AUTO_RELOAD'] = True
 load_dotenv()
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('POSTGRES_URI')
+app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-db = SQLAlchemy(app)
+conn = psycopg2.connect(os.environ['PGDSN'])
+
+actors_query = 'SELECT actor_id, first_name, last_name FROM actor LIMIT 20'
 
 
 # ----- Home page
@@ -22,26 +20,28 @@ def homepage_index():
     return render_template('homepage.jinja2', title=title)
 
 
-# -----  Actors
-class Actor(db.Model):
-    actor_id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(50))
-    last_name = db.Column(db.String(50))
-
-
 @app.route('/actors/')
 def actors_index():
-    actors = db.session.execute(text('SELECT * from actor LIMIT 20')).fetchall()
-    title = f'Actors'
-    return render_template('actors.jinja2', title=title, actors=actors)
+    with conn.cursor() as cur:
+        cur.execute(actors_query)
+        actors = cur.fetchall()
+        cur.close()
+
+        title = f'Actors'
+        return render_template('actors.jinja2', title=title, actors=actors)
 
 
 @app.route('/actors/<int:actor_id>')
 def actor_index(actor_id):
-    actors = db.session.execute(text('SELECT * from actor LIMIT 20')).fetchall()
-    query_string = 'SELECT * from actor WHERE actor_id = :actor_id'
-    query = text(query_string).bindparams(actor_id=actor_id)
-    result = db.session.execute(query)
-    actor = result.first()
-    title = f'Actor: {actor.first_name} {actor.last_name}'
-    return render_template('actor.jinja2', actors=actors, actor=actor, title=title)
+    with conn.cursor() as cur:
+        cur.execute(actors_query)
+        actors = cur.fetchall()
+
+        actor_query = 'SELECT * from actor WHERE actor_id = %s'
+        cur.execute(actor_query, (actor_id,))
+        actor = cur.fetchone()
+
+        cur.close()
+
+        title = f'Actor: {actor[1]} {actor[2]}'
+        return render_template('actor.jinja2', actors=actors, actor=actor, title=title)
